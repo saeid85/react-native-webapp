64ea26c25f90de8be7d268dec5ef37aa83ce0aaa
Saeid Saberi <s.saberi@yahoo.com>
2019-02-07 16:01:07 +0800

    HomeScreen & Config files have been added to the app. App displays the web address provided in the config file. Header can be hide/displayed based on the user preference easily by changing -visible- option in the config file.

---
+++ config.js
@@ -0,0 +1,28 @@
+export default (config = {
+  header: {
+    visible: true, // false: hide the heade NavBar
+    hasLogo: true, // false: hide logo
+    logoMarginLeft: 0,
+    logoMarginRight: 16,
+    logoHeight: 24,
+    logoWidth: 24,
+    title: "RN WebApp",
+    titleColor: "#000",
+    backgroundColor: "#f70",
+    paddingLeft: 16
+  },
+  spinner: {
+    showSpinner: true, // false: hide the spinner on loading the page
+    color: "#f00", // Spinner color
+    size: "large", // small OR large
+    positionRight: 10,
+    positionBottom: 10,
+  },
+  web: {
+    url: "https://github.com/",
+    marginTop: 0,
+    automaticallyAdjustContentInsets: true,  // Set this to provide JavaScript that will be injected into the web page when the view loads. Make sure the string evaluates to a valid type (true works) and doesn't otherwise throw an exception.
+    cacheEnabled: true, // Sets whether WebView & WKWebView should use browser caching.
+    scalesPageToFit: true // Boolean that controls whether the web content is scaled to fit the view and enables the user to change the scale.
+  }
+});
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import { ActivityIndicator, Image, StyleSheet, View } from "react-native";
+import { Container, Header, Title, Body } from "native-base";
+import { WebView } from "react-native-webview";
+import config from "../../config";
+
+export default class HomeScreen extends Component<Props> {
+  state = {
+    isLoading: false
+  };
+
+  render() {
+    const { header, spinner, web } = config;
+
+    return (
+      <Container>
+        {header.visible && (
+          <Header style={styles.header}>
+            <Body style={styles.headerBody}>
+              {header.hasLogo && (
+                <Image
+                  source={require("../assets/logo.png")}
+                  style={styles.logo}
+                />
+              )}
+              <Title style={styles.headerTitle}>
+                {header.title}
+                {this.state.isLoading}
+              </Title>
+            </Body>
+          </Header>
+        )}
+        {spinner.showSpinner &&
+          this.state.isLoading && (
+            <ActivityIndicator
+              color={spinner.color}
+              size={spinner.size}
+              style={styles.spinner}
+            />
+          )}
+        <WebView
+          cacheEnabled={web.cacheEnabled}
+          automaticallyAdjustContentInsets={
+            web.automaticallyAdjustContentInsets
+          }
+          source={{ uri: web.url }}
+          style={styles.webview}
+          onLoadStart={() => this.setState({ isLoading: true })}
+          onLoadEnd={() => this.setState({ isLoading: false })}
+        />
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: config.header.backgroundColor,
+    paddingLeft: config.header.paddingLeft
+  },
+  headerBody: {
+    flexDirection: config.header.hasLogo ? "row" : "column"
+  },
+  headerTitle: {
+    color: config.header.titleColor
+  },
+  logo: {
+    width: config.header.logoWidth,
+    height: config.header.logoHeight,
+    marginLeft: config.header.logoMarginLeft,
+    marginRight: config.header.logoMarginRight
+  },
+  spinner: {
+    position: "absolute",
+    right: config.spinner.positionRight,
+    bottom: config.spinner.positionBottom,
+    zIndex: 10
+  },
+  webview: {
+    marginTop: config.web.marginTop
+  }
+});
